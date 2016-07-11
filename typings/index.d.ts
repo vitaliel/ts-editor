d535cf0ff7c592f4f85839c95e03c1dbbc1e9353
Vitalie Lazu <vitalie.lazu@gmail.com>
2016-07-11 10:22:25 +0300

    Added react-redux

--- typings/index.d.ts
+++ typings/index.d.ts
@@ -1,2 +1,4 @@
 /// <reference path="globals/react-dom/index.d.ts" />
+/// <reference path="globals/react-redux/index.d.ts" />
 /// <reference path="globals/react/index.d.ts" />
+/// <reference path="globals/redux/index.d.ts" />
