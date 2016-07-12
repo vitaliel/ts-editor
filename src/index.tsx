aaf38cd311e76492b396a0cf3e72e830c307feb2
Vitalie Lazu <vitalie.lazu@gmail.com>
2016-07-12 17:47:15 +0300

    Clean-up

--- src/index.tsx
+++ src/index.tsx
@@ -1,9 +1,6 @@
 import * as React from "react";
 import * as ReactDOM from "react-dom";
 
-import { Hello } from "./components/Hello";
-import { Timer } from "./components/Timer";
-
 import EditorApp from "./components/EditorApp";
 
 import { Provider } from 'react-redux'
@@ -18,14 +15,3 @@ ReactDOM.render(
     </Provider>,
     document.getElementById("editor")
 );
-
-// ReactDOM.render(
-//     <Timer />,
-//     document.getElementById("timer")
-// );
-//
-// ReactDOM.render(
-//     <Hello compiler="TypeScript" framework="React" />,
-//     document.getElementById("hello_example")
-// );
-//
